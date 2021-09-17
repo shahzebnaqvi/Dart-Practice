76f0541b1489a2cd8f9474ac11008b9f0db2ae3e
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-17 17:55:53 +0500

    Question 1

---
+++ 1 _ various types of operators.dart
@@ -0,0 +1,35 @@
+void main() {
+  print ("Three Type of Operators");
+  print("1) Arithmetic Operators\n2) Equality and Relational Operators\n3) Logical Operators");
+  var a=5;
+  var b=3;
+  print("Value of a = $a and b = $b");
+  print("------------------------------------------");
+  print("1 ) Arithmetic Operators : - ");
+  print("------------------------------------------");
+  print("This is Add Operator : a + b =  ${a+b}");
+  print("This is Subtract Operator : a - b =  ${a-b}");
+  print("This is Multiply Operator : a * b =  ${a*b}");
+  print("This is Divide Operator : a / b =  ${a/b}");
+  print("This is Integer Result : a ~/ b =  ${a~/b}");
+  print("This is Modulus : a % b =  ${a%b}");
+  print("This is Increment : ++a =  ${++a}");
+  print("This is Decrement : --a =  ${--a}");
+  print("------------------------------------------");
+  print("2 ) Equality and Relational Operators : - ");
+  print("------------------------------------------");
+  print("This is Greater Than : a > b =  ${a>b}");
+  print("This is Less Than : a < b =  ${a<b}");
+  print("This is Greater Than or equal to : a >= b =  ${a>=b}");
+  print("This is Less Than or equal to : a <= b =  ${a<=b}");
+  print("This is Equality : a == b =  ${a==b}");
+  print("This is Not Equal : a != b =  ${a!=b}");
+  print("------------------------------------------");
+  print("3 ) Logical Operators : - ");
+  print("------------------------------------------");
+  print("This is AND Operator : a == 3 && b == 4 =  ${a== 3 && b ==4}");
+  print("This is OR Operator : a == 3 || b == 4 =  ${a== 3 || b ==4}");
+  print("This is NOT Operator : !(a== 3) =  ${!(a== 3)}");
+  
+  
+}
