33430c784bc8cfff4c5c3bf0bb322ebba03b22b8
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-18 10:02:01 +0500

    Question 9

---
+++ 9_marksheet_using_array_list.dart
@@ -0,0 +1,8 @@
+void main() {
+  var students_name = ["Ali","Furqan" ,"Bilal"];
+  var student_score = [400,340,490];
+  print("Student ---------- Score ---------- Percentage");
+  print("${students_name[0]} --------------- ${student_score[0]} ---------- ${student_score[0]/500*100}");
+  print("${students_name[1]} ------------ ${student_score[1]} ---------- ${student_score[1]/500*100}");
+  print("${students_name[2]} ------------ ${student_score[2]} ---------- ${student_score[2]/500*100}");
+}
