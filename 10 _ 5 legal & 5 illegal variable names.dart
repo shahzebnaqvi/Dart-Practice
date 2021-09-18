dfa3e31700c52d03c749eb94b785a97cdd32702e
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-18 10:02:48 +0500

    Question 10

---
+++ 10 _ 5 legal & 5 illegal variable names.dart
@@ -0,0 +1,16 @@
+void main() {
+//   5 legal & 5 illegal variable names
+ // 5 legal  variable names
+  var legal1= "abc";
+  var legal2= "a23c";
+  var legal3= "ab23";
+  var legal4= "abc23";
+  var legal5= "ab2323c";
+// 5 illegal variable names
+   var print = "abc";
+  var num= "a23c";
+  var String= "ab23";
+  var 8_sdf= "abc23";
+  var if= "ab2323c";
+print(print);
+}
