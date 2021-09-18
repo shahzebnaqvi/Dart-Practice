85cd144cde7d072d20b536bd54bb3a073ea39eea
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-19 00:44:15 +0500

    Question 5

---
+++ 5_ operators questionmark and doublequestionmark.dart
@@ -0,0 +1,5 @@
+
+void main() {
+     print("? Operator : \nIt is a simple version of if-else statement. If the condition is true than expersion1 is executed else expersion2 is executed.");
+     print ("?? Operator : \nIf expersion1 is non-null returns its value else returns expression2 value.");
+}
